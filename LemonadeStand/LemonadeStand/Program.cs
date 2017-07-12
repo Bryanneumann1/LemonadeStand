4c7296d14819b153852c7490a02e1d52f3a53c7a
Bryan Neumann <Bryan Neumann>
2017-07-12 10:19:34 -0500

    created classes and rules

---
+++ LemonadeStand/LemonadeStand/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your Lemonade Stand");
+            rules Start = new rules();
+            Start.DisplayRules();
+                
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LemonadeStand/LemonadeStand/rules.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class rules
+    {
+
+        public rules()
+        {
+
+        }
+        public void DisplayRules()
+        {
+            Console.WriteLine("Rules for Lemonade stand are:\n" +
+                "You will be running your very own lemonade stand\n" +
+                "You will have to manage your invintory\n" +
+                "Weather will have an effect on your daily sales\n"+
+                "Sales will increase for hot dry days and decrease on colder rainy days\n" +
+                "Set your price accordingly according to the weather\n" +
+                "So be sure to check the weather everyday\n" +
+                "At the end of 7 days you will see how much lemonade you have sold");
+        }
+    }
+}
